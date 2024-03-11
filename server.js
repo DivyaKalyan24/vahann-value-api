2ee1450c56a9e2954f73d1898e82421214464488
DivyaKalyan24 <dkandru@gitam.in>
2024-03-12 00:49:14 +0530

    .

--- server.js
+++ server.js
@@ -36,8 +36,7 @@ app.use(express.json())
 
 app.use(
     cors({
-        origin: 'https://vahann-value.vercel.app',
-        methods: ['POST', 'GET'],
+        origin: '*',
         credentials: true
     })
 )
